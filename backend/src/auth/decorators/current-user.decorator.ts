03feec8367a03a6d40dcbde8a67c93d7c32e87b3
ayoub.labite@gmail.com <ayoub.labite@gmail.com>
2026-03-05 13:28:50 +0000

    SIH-97 Créer decorators (Roles, CurrentUser)

--- backend/src/auth/decorators/current-user.decorator.ts
+++ backend/src/auth/decorators/current-user.decorator.ts
@@ -1,4 +1,8 @@
-import { SetMetadata } from '@nestjs/common';
-import { UserRole } from '../../common/enums/role.enum';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const Roles = (...roles: UserRole[]) => SetMetadata('roles', roles);
+export const CurrentUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user;
+  },
+);
