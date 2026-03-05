ce0ed768d92ee6c7b990676208b122f73a2fd136
ayoub.labite@gmail.com <ayoub.labite@gmail.com>
2026-03-05 23:41:08 +0000

    SIH-111 Créer DTOs (CreateUserDto, UpdateUserDto)

---
+++ backend/src/users/dto/update-user.dto.ts
@@ -0,0 +1,27 @@
+import { IsString, IsOptional, IsBoolean } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsString()
+  @IsOptional()
+  firstName?: string;
+
+  @IsString()
+  @IsOptional()
+  lastName?: string;
+
+  @IsString()
+  @IsOptional()
+  phone?: string;
+
+  @IsString()
+  @IsOptional()
+  address?: string;
+
+  @IsString()
+  @IsOptional()
+  specialite?: string;
+
+  @IsBoolean()
+  @IsOptional()
+  isActive?: boolean;
+}
